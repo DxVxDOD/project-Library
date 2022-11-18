9cff0e02c47fa88a2c086f8ddb0b26ecf92f7974
David Orbang <orbangdavid@yahoo.com>
2022-11-18 16:05:46 +0000

    basic html and css done js data retrive from forms done

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let myLibrary = [];
-
 class Book {
     constructor(author, pages, title, read) {
         this.author = author,
@@ -10,4 +8,26 @@ class Book {
 };
 
 const lotr = new Book("JRRT", "312321", "TLOTR", "Yes");
+const currentBooks = document.querySelector(".current-books");
+const btn = document.querySelector(".button");
 
+function makeDiv(e) {
+    e.preventDefault();
+    let cell = document.createElement("div");
+    currentBooks.appendChild(cell).className = "book";
+}
+
+function getData(form) {
+    let formData = new FormData(form);
+
+    // iterate trough enteries
+    for (let pair of formData.entries()) {
+        console.log(pair[0] + ":" + pair[1]);
+    }
+}
+
+document.getElementById("myForm").addEventListener("submit",
+function(e) {
+    e.preventDefault();
+    getData(e.target);
+});
