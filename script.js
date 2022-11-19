0157dad660014e6c4f27c622677f4a3593496d1b
David Orbang <orbangdavid@yahoo.com>
2022-11-19 15:52:47 +0000

    js logic done

--- script.js
+++ script.js
@@ -25,17 +25,34 @@ function displayBooksOnPage() {
             console.log(`${key}: ${myLibrary[key]}`);
             const para = document.createElement("p");
             para.textContent = (`${key}: ${myLibrary[key]}`);
-            card.appendChild(para)
+            card.appendChild(para);
+            console.log(myLibrary);
         }
     })
+    
 }
 
+function getData(form) {
+    const formData = new FormData(form);
+    const books = document.querySelector(".books");
+    const card2 = document.createElement("div");
+    card2.classList.add("card");
+    books.appendChild(card2);
 
+    for (var pair of formData.entries()) {
+        const para2 = document.createElement("p");
+        para2.textContent = (pair[0] + ": " + pair[1]);
+        card2.appendChild(para2)
+    }
+    console.log(Object.fromEntries(formData));
+}
+
+document.getElementById("myForm").addEventListener("submit", 
+function(e) {
+    e.preventDefault();
+    getData(e.target);
+});
 
 addBookToLibrary("mark mark", "86530", "uhqwjkn", "YES");
-addBookToLibrary("mark mark", "86530", "uhqwjkn", "YES");
-addBookToLibrary("mark mark", "86530", "uhqwjkn", "YES");
-
-console.log("end of contents", myLibrary);
 
 displayBooksOnPage();
